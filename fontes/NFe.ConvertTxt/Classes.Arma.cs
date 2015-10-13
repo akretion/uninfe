73079b1491ae56f74e7ad51d95814848260a3fe3
wandreymf <wandreymf@ad8285b1-d111-48d0-abe1-d7f9e4f2854c>
2015-10-13 20:55:36 +0000

    Ajuste no repositório para disponibilizar tanto a versão 5.0 do UniNFe como a 4.6

---
+++ fontes/NFe.ConvertTxt/Classes.Arma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.ConvertTxt
+{
+    /// <summary>
+    /// Arma
+    /// </summary>
+    public class Arma
+    {
+        public TpcnTipoArma tpArma;
+        public int nSerie;
+        public int nCano;
+        public string descr;
+    }
+}
---
+++ fontes/NFe.ConvertTxt/Classes.comb_cide.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.ConvertTxt
+{
+    /// <summary>
+    /// Comb
+    /// </summary>
+    public struct Comb
+    {
+        public int cProdANP;
+        public double pMixGN;
+        public string CODIF;
+        public double qTemp;
+        public string UFCons;
+        public CIDE CIDE;
+    }
+
+    /// <summary>
+    /// CIDE
+    /// </summary>
+    public struct CIDE
+    {
+        public double qBCprod;
+        public double vAliqProd;
+        public double vCIDE;
+    }
+
+}
