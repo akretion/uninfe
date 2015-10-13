ac136243e167b86b7fef21b8b4bc0c37a729efb7
wandreymf <wandreymf@ad8285b1-d111-48d0-abe1-d7f9e4f2854c>
2015-10-13 20:55:36 +0000

    Ajuste no repositório para disponibilizar tanto a versão 5.0 do UniNFe como a 4.6

---
+++ branches/build_dotnet35/fontes/NFe.ConvertTxt/Classes.obsFisco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.ConvertTxt
+{
+    /// <summary>
+    /// obsFisco
+    /// </summary>
+    public class obsFisco
+    {
+        public string xCampo;
+        public string xTexto;
+    }
+}
