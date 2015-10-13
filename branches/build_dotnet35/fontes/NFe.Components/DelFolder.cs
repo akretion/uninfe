86d6fab953c3fa0a06d076bed92f6555097325d7
wandreymf <wandreymf@ad8285b1-d111-48d0-abe1-d7f9e4f2854c>
2015-10-13 20:45:51 +0000

    Ajuste no repositório para disponibilizar tanto a versão 5.0 do UniNFe como a 4.6

---
+++ branches/build_dotnet35/fontes/NFe.Components/DelFolder.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace NFe.Components
+{
+    internal class DeltreeData
+    {
+        public string PathName { get; set; }
+        public List<string> Files { get; set; }
+
+        public DeltreeData()
+        {
+            Files = new List<string>();
+        }
+    }
+
+    public sealed class DeltreeMain
+    {
+        #region Fields
+        private static readonly Stack<DeltreeData> StackTree = new Stack<DeltreeData>();
+        private static bool _quiet;
+        #endregion
+
+        #region Begin delete tree
+        public static void BeginDeleteTree(string path, bool quiet)
+        {
+            _quiet = quiet;
+
+            if (!string.IsNullOrEmpty(path))
+                if (Directory.Exists(path))
+                    DeltreeThreadCallback(path);
+        }
+
+        private static void DeltreeThreadCallback(string path)
+        {
+            /* We first must get our directory and file structure */
+            if (BuildDirectoryList(path))
+            {
+                DeleteTree();
+                if (Directory.Exists(path))
+                    System.Threading.Thread.Sleep(500);
+            }
+        }
+        #endregion
+
+        #region Build directory and file list
+        private static bool BuildDirectoryList(string path)
+        {
+#if DEBUG
+            Console.WriteLine(Environment.NewLine + "DELTREE deleting tree structure " + "'" + path + "' ..." + Environment.NewLine);
+#endif
+            try
+            {
+                var directories = new List<string> { path };
+                directories.AddRange(Directory.GetDirectories(path, "*", SearchOption.AllDirectories));
+                foreach (var d in directories)
+                {
+                    var dtData = new DeltreeData { PathName = d };
+                    dtData.Files.AddRange(Directory.GetFiles(d, "*.*"));
+                    StackTree.Push(dtData);
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Console.WriteLine("DELTREE error: " + ex.Message);
+                return false;
+#else
+                if (!_quiet)
+                    throw ex;
+#endif
+            }
+            return true;
+        }
+        #endregion
+
+        #region Delete tree
+        private static void DeleteTree()
+        {
+            /* Nothing to do */
+            if (StackTree.Count == 0)
+            {
+#if DEBUG
+                Console.WriteLine("DELTREE error: Nothing to delete!");
+#endif
+                return;
+            }
+            var fileCount = 0;
+            var directoryCount = 0;
+            while (StackTree.Count > 0)
+            {
+                /* Get first item on the stack */
+                var dtData = StackTree.Pop();
+                /* Shouldn't happen .... */
+                if (dtData == null) { continue; }
+                if (dtData.Files.Count > 0)
+                {
+                    foreach (var f in dtData.Files)
+                    {
+#if DEBUG
+                        if (!_quiet) { Console.WriteLine("Deleting file: " + f); }
+#endif
+                        try
+                        {
+                            File.Delete(f);
+                        }
+                        catch (Exception ex)
+                        {
+#if DEBUG
+                            Console.WriteLine("DELTREE error: " + ex.Message);
+                            return;
+#else
+                            if (!_quiet)
+                                throw ex;
+#endif
+                        }
+                        ++fileCount;
+                    }
+                }
+
+#if DEBUG
+                /* Remove directory */
+                if (!_quiet)
+                {
+                    Console.WriteLine("Removing directory: " + dtData.PathName);
+                }
+#endif
+                try
+                {
+                    Directory.Delete(dtData.PathName);
+                }
+                catch (Exception ex)
+                {
+#if DEBUG
+                    Console.WriteLine("DELTREE error: " + ex.Message);
+                    return;
+#else
+                    if (!_quiet)
+                        throw ex;
+#endif
+                }
+                ++directoryCount;
+            }
+#if DEBUG
+            Console.WriteLine(
+                (!_quiet ? Environment.NewLine : null) +
+                "DELTREE removed {0} file(s) and {1} directory(ies) ({2} total objects removed)",
+                fileCount, directoryCount, fileCount + directoryCount);
+#endif
+        }
+        #endregion
+    }
+}
---
+++ branches/build_dotnet35/fontes/NFe.Components/Fiorilli/Fiorilli.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.Components.Fiorilli
+{
+    public class Fiorilli : FiorilliBase
+    {
+        #region Construtures
+        public Fiorilli(TipoAmbiente tpAmb, string pastaRetorno, int codMun, string usuario, string senhaWs, string proxyuser, string proxypass, string proxyserver)
+            : base(tpAmb, pastaRetorno, codMun, usuario, senhaWs, proxyuser, proxypass, proxyserver)
+        { }
+        #endregion
+    }
+}
