eba9f6bc804d2af748be99bb28a7c10d21918075
wandreymf <wandreymf@ad8285b1-d111-48d0-abe1-d7f9e4f2854c>
2015-10-13 21:30:33 +0000

    Ajuste no repositório para disponibilizar tanto a versão 5.0 do UniNFe como a 4.6

---
+++ fontes/MetroFramework/MetroFramework.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MetroFramework.Demo
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+#if DEBUG
+            NFe.Components.NativeMethods.AllocConsole();
+#endif
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+#if DEBUG
+            NFe.Components.NativeMethods.FreeConsole();
+#endif
+        }
+    }
+}
