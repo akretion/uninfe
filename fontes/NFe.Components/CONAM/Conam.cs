0e10a56695f514ab6964f46d57a36b4d743ffe7d
wandreymf <wandreymf@ad8285b1-d111-48d0-abe1-d7f9e4f2854c>
2015-10-13 21:31:48 +0000

    Ajuste no repositório para disponibilizar tanto a versão 5.0 do UniNFe como a 4.6
    git-svn-id: https://svn.code.sf.net/p/uninfe/code/trunk@278 ad8285b1-d111-48d0-abe1-d7f9e4f2854c

---
+++ fontes/NFe.Components/CONAM/Conam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.Components.Conam
+{
+    public class Conam : ConamBase
+    {
+        #region Construtures
+        public Conam(TipoAmbiente tpAmb, string pastaRetorno, int codMun, string usuario, string senhaWs)
+            : base(tpAmb, pastaRetorno, codMun, usuario, senhaWs)
+        { }
+        #endregion
+    }
+}
---
+++ fontes/NFe.Components/FolderCompare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.Components
+{
+    #region Classe interna para comparar as pastas informadas nas configurações do UniNFe
+    /// <summary>
+    /// danasa 8-2009
+    /// classe interna para comparar as pastas informadas
+    /// </summary>
+    /// 
+    /*
+    public class FolderCompare
+    {
+        public Int32 id { get; set; }
+        public string folder { get; set; }
+
+        public FolderCompare(Int32 _id, string _folder)
+        {
+            this.id = _id;
+            this.folder = _folder;
+        }
+    }*/
+    #endregion
+}
