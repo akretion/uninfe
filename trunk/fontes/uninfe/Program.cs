4a3f3aafa14554f34efbeec7e0cfc8c8aeff96e4
wandreymf <wandreymf@ad8285b1-d111-48d0-abe1-d7f9e4f2854c>
2015-10-14 10:45:40 +0000

    Ajuste no repositório para disponibilizar tanto a versão 5.0 do UniNFe como a 4.6

---
+++ trunk/fontes/uninfe/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Xml;
+using System.Xml.Linq;
+using NFe.Components;
+using NFe.Components.Info;
+using NFe.Settings;
+
+namespace uninfe
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler((sender, e) =>
+            {
+                Auxiliar.WriteLog(e.Exception.Message + "\r\n" + e.Exception.StackTrace, false);
+                if (e.Exception.InnerException != null)
+                {
+                    Auxiliar.WriteLog(e.Exception.InnerException.Message + "\r\n" + e.Exception.InnerException.StackTrace, false);
+
+                    if (e.Exception.InnerException.InnerException != null)
+                    {
+                        Auxiliar.WriteLog(e.Exception.InnerException.InnerException.Message + "\r\n" + e.Exception.InnerException.InnerException.StackTrace, false);
+                    }
+
+                    if (e.Exception.InnerException.InnerException.InnerException != null)
+                    {
+                        Auxiliar.WriteLog(e.Exception.InnerException.InnerException.InnerException.Message + "\r\n" + e.Exception.InnerException.InnerException.InnerException.StackTrace, false);
+                    }
+                }
+            });
+
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler((sender, e) =>
+            {
+                Auxiliar.WriteLog(e.ExceptionObject.ToString(), false);
+            });
+
+            //Esta deve ser a primeira linha do Main, não coloque nada antes dela. Wandrey 31/07/2009
+            Propriedade.AssemblyEXE = Assembly.GetExecutingAssembly();
+
+            bool silencioso = false;
+            ConfiguracaoApp.AtualizaWSDL = false;
+            
+            //Começar a contar o tempo de execução do aplicativo - Renan 24/06/2015
+            ConfiguracaoApp.ExecutionTime = new System.Diagnostics.Stopwatch();
+            ConfiguracaoApp.ExecutionTime.Start();
+
+            if (args.Length >= 1)
+                foreach (string param in args)
+                {
+                    if (param.ToLower().Equals("/silent"))
+                    {
+                        silencioso = true;
+                        continue;
+                    }
+                    if (param.ToLower().Equals("/updatewsdl"))
+                    {
+                        ConfiguracaoApp.AtualizaWSDL = true;
+                        continue;
+                    }
+                    if (param.ToLower().Equals("/quit") || param.ToLower().Equals("/restart"))
+                    {
+                        string procName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+                        int Id = System.Diagnostics.Process.GetCurrentProcess().Id;
+
+                        foreach (System.Diagnostics.Process clsProcess in System.Diagnostics.Process.GetProcesses())
+                        {
+                            if (clsProcess.ProcessName.Equals(procName))
+                            {
+                                try
+                                {
+                                    if (param.ToLower().Equals("/quit") ||
+                                        (param.ToLower().Equals("/restart") && clsProcess.Id != Id))
+                                    {
+                                        Empresas.ClearLockFiles(false);
+                                        clsProcess.Kill();
+                                    }
+                                }
+                                catch
+                                {
+                                }
+                                if (param.ToLower().Equals("/quit"))
+                                    return;
+                            }
+                        }
+                    }
+                }
+
+            Propriedade.TipoAplicativo = TipoAplicativo.Todos;
+
+#if DEBUG
+            NFe.Components.NativeMethods.AllocConsole();
+            Console.WriteLine("start....." + Propriedade.NomeAplicacao);
+#endif
+
+            bool executando = Aplicacao.AppExecutando();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (executando)
+            {
+                if (!silencioso)
+                    MetroFramework.MetroMessageBox.Show(null,
+                        "Somente uma instância do " + Propriedade.NomeAplicacao + " pode ser executada." + (Empresas.ExisteErroDiretorio ? "\r\nPossíveis erros:\r\n" + Empresas.ErroCaminhoDiretorio : ""), "",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (!silencioso)
+                    if (Empresas.ExisteErroDiretorio)
+                        MetroFramework.MetroMessageBox.Show(null,
+                                "Ocorreu um erro ao efetuar a leitura das configurações da empresa. " +
+                                "Por favor entre na tela de configurações da(s) empresa(s) listada(s), acesse a aba \"Pastas\" e reconfigure-as.\r\n\r\n" + Empresas.ErroCaminhoDiretorio, "",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                Application.Run(new NFe.UI.Form_Main());
+            }
+#if DEBUG
+            NFe.Components.NativeMethods.FreeConsole();
+#endif
+        }
+
+    }
+
+}
