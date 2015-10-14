82bef2ed71b9f2a076230411cd864b38c7e1183e
wandreymf <wandreymf@ad8285b1-d111-48d0-abe1-d7f9e4f2854c>
2015-10-14 10:05:01 +0000

    Ajuste no repositório para disponibilizar tanto a versão 5.0 do UniNFe como a 4.6

---
+++ fontes/NFe.ConvertTxt/Classes.Cana.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.ConvertTxt
+{
+    /// <summary>
+    /// Cana
+    /// </summary>
+    public class Cana
+    {
+        public string safra;
+        public string Ref;
+        public double qTotMes;
+        public TpcnTipoCampo qTotMes_Tipo;
+        public double qTotAnt;
+        public TpcnTipoCampo qTotAnt_Tipo;
+        public double qTotGer;
+        public TpcnTipoCampo qTotGer_Tipo;
+        public double vFor;
+        public double vTotDed;
+        public double vLiqFor;
+        public List<fordia> fordia;
+        public List<deduc> deduc;
+
+        public Cana()
+        {
+            qTotMes_Tipo = TpcnTipoCampo.tcDec10;
+            qTotAnt_Tipo = TpcnTipoCampo.tcDec10;
+            qTotGer_Tipo = TpcnTipoCampo.tcDec10;
+            fordia = new List<fordia>();
+            deduc = new List<deduc>();
+        }
+    }
+}
---
+++ fontes/NFe.ConvertTxt/Classes.Cobr.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.ConvertTxt
+{
+    /// <summary>
+    /// Cobr
+    /// </summary>
+    public class Cobr
+    {
+        public Fat Fat;
+        public List<Dup> Dup;
+
+        public Cobr()
+        {
+            Dup = new List<Dup>();
+        }
+    }
+
+    /// <summary>
+    /// Dup
+    /// </summary>
+    public class Dup
+    {
+        public string nDup;
+        public DateTime dVenc;
+        public double vDup;
+    }
+
+    /// <summary>
+    /// Fat
+    /// </summary>
+    public struct Fat
+    {
+        public string nFat;
+        public double vOrig;
+        public double vDesc;
+        public double vLiq;
+    }
+
+}
