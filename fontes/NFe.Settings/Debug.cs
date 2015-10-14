096273314e491bfc53eed686fd73350f780a8486
wandreymf <wandreymf@ad8285b1-d111-48d0-abe1-d7f9e4f2854c>
2015-10-14 10:23:44 +0000

    Ajuste no repositório para disponibilizar tanto a versão 5.0 do UniNFe como a 4.6

---
+++ fontes/NFe.Settings/Debug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFe.Settings
+{
+#if DEBUG
+    /// <summary>
+    /// Classe para mensagem em modo debug, que serão exibidas no output
+    /// </summary>
+    public sealed class Debug
+    {
+        #region Constantes
+        /// <summary>
+        /// Categoria para o modo debug
+        /// </summary>
+        public const string Category = "UniNFe";
+        #endregion
+
+        /// <summary>
+        /// Escreve um log no Output
+        /// </summary>
+        /// <param name="message">mensagem a ser escrita</param>
+        public static void WriteLine(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(String.Format("{0:HH:mm:ss.fff}=> {1}", DateTime.Now, message), Category);
+        }
+    }
+
+#endif
+}
